9bd052ff60536627a6bcd8f0ee78f82f869b6eba
KahlerRockwell <Rockwell1023@gmail.com>
2020-04-08 11:14:39 -0400

    adds antlr4 with parser file

---
+++ CompilerPiper/parser.js
@@ -0,0 +1,56 @@
+"use strict";
+exports.__esModule = true;
+var Token_1 = require("./Token");
+var NodeType_1 = require("./NodeType");
+var antlr4 = require('./antlr4');
+var Lexer = require('./gramLexer.js').gramLexer;
+var Parser = require('./gramParser.js').gramParser;
+var ErrorHandler = /** @class */ (function () {
+    function ErrorHandler() {
+    }
+    ErrorHandler.prototype.syntaxError = function (rec, sym, line, column, msg, e) {
+        console.log("Syntax Error: ", msg, " on line", line, " at column ", column);
+        throw new Error("Syntax error in ANTLR parse");
+    };
+    return ErrorHandler;
+}());
+function parse(txt) {
+    var stream = new antlr4.InputStream(txt);
+    var lexer = new Lexer(stream);
+    var tokens = new antlr4.CommonTokenStream(lexer);
+    var parser = new Parser(tokens);
+    parser.buildParseTrees = true;
+    var handler = new ErrorHandler();
+    lexer.removeErrorListeners();
+    lexer.addErrorListener(handler);
+    parser.removeErrorListeners();
+    parser.addErrorListener(handler);
+    var antlrroot = parser.program();
+    var root = walk(parser, antlrroot);
+    return root.toString();
+}
+exports.parse = parse;
+function walk(parser, node) {
+    var p = node.getPayload();
+    if (p.ruleIndex === undefined) {
+        var line = p.line;
+        var lexeme = p.text;
+        var ty = p.type;
+        var sym = parser.symbolicNames[ty];
+        if (sym === null) {
+            sym = lexeme.toUpperCase();
+        }
+        var T = new Token_1.Token(sym, lexeme, line);
+        return new NodeType_1.TreeNode(sym, T);
+    }
+    else {
+        var idx = p.ruleIndex;
+        var sym = parser.ruleNames[idx];
+        var N = new NodeType_1.TreeNode(sym, undefined);
+        for (var i = 0; i < node.getChildCount(); ++i) {
+            var child = node.getChild(i);
+            N.children.push(walk(parser, child));
+        }
+        return N;
+    }
+}
---
+++ CompilerPiper/parser.ts
@@ -0,0 +1,60 @@
+import { Token } from './Token';
+import { TreeNode } from './NodeType';
+
+declare var require: any;
+let antlr4 = require('./antlr4');
+let Lexer = require('./gramLexer.js').gramLexer;
+let Parser = require('./gramParser.js').gramParser;
+
+class ErrorHandler {
+    syntaxError(rec: any, sym: any, line: number,
+        column: number, msg: string, e: any) {
+        console.log("Syntax Error: ", msg, " on line", line,
+            " at column ", column);
+        throw new Error("Syntax error in ANTLR parse");
+    }
+}
+
+export function parse(txt: string): string {
+    let stream = new antlr4.InputStream(txt);
+    let lexer = new Lexer(stream);
+    let tokens = new antlr4.CommonTokenStream(lexer);
+    let parser = new Parser(tokens);
+    parser.buildParseTrees = true;
+
+    let handler = new ErrorHandler();
+    lexer.removeErrorListeners();
+    lexer.addErrorListener(handler);
+    parser.removeErrorListeners();
+    parser.addErrorListener(handler);
+    let antlrroot = parser.program();
+
+    let root: TreeNode = walk(parser, antlrroot);
+
+    return root.toString();
+}
+
+function walk(parser: any, node: any) {
+    let p: any = node.getPayload();
+    if (p.ruleIndex === undefined) {
+        let line: number = p.line;
+        let lexeme: string = p.text;
+        let ty: number = p.type;
+        let sym: string = parser.symbolicNames[ty];
+        if (sym === null) {
+            sym = lexeme.toUpperCase();
+        }
+        let T = new Token(sym, lexeme, line);
+        return new TreeNode(sym, T);
+    } else {
+        let idx: number = p.ruleIndex;
+        let sym: string = parser.ruleNames[idx];
+        let N = new TreeNode(sym, undefined);
+
+        for (let i = 0; i < node.getChildCount(); ++i) {
+            let child: any = node.getChild(i);
+            N.children.push(walk(parser, child));
+        }
+        return N;
+    }
+}
